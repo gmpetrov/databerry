8966e658fe53418100ebc08e628c46e7be3a4ada
Georges Petrov <georgesm.petrov@gmail.com>
2023-03-28 21:17:43 +0200

    📈 add ga

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document';
+import Script from 'next/script';
 
 class CustomDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -27,6 +28,20 @@ class CustomDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
             rel="stylesheet"
           />
+
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-QTN3QP6PKB"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-QTN3QP6PKB');
+        `}
+          </Script>
         </Head>
         <body>
           {getInitColorSchemeScript()}
