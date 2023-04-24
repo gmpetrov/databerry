295d09f981893b5d3698169b67d77fbca3ea9b46
Georges Petrov <georgesm.petrov@gmail.com>
2023-04-24 11:35:32 +0200

    disable for influencers section

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,7 +58,7 @@ export default function Home() {
         <Faqs /> */}
         <FeaturesForCustomerSupport />
         <FeaturesForSlack />
-        <FeaturesForInfluencers />
+        {/* <FeaturesForInfluencers /> */}
         <FeaturesForDevs />
         <ChatBotBenefits />
         <FAQ />
