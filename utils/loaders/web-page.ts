189fa0ca197a3ef6d94f6889aaaa2a780a4da705
Georges Petrov <georgesm.petrov@gmail.com>
2023-04-26 11:03:39 +0200

    Revert "Revert "use playwright as fallback when getting 403 erors while crawling webpage""
    This reverts commit c9ba72725816061b60c39485ba5b884fb03563bf.

--- utils/loaders/web-page.ts
+++ utils/loaders/web-page.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import playwright from 'playwright';
 import { z } from 'zod';
 
 import { WebPageSourceSchema } from '@app/components/DatasourceForms/WebPageForm';
@@ -7,6 +8,43 @@ import type { Document } from '@app/utils/datastores/base';
 import addSlashUrl from '../add-slash-url';
 
 import { DatasourceLoaderBase } from './base';
+
+const loadPageContent = async (url: string) => {
+  const urlWithSlash = addSlashUrl(url);
+
+  try {
+    const { data } = await axios(urlWithSlash, {
+      headers: {
+        'User-Agent': Date.now().toString(),
+      },
+    });
+
+    return data as string;
+  } catch (err) {
+    console.log('Error: Trying Plawright fallback');
+    // const { default: playwright } = await import('playwright');
+
+    const customUserAgent =
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36';
+
+    const browser = await playwright.chromium.launch({
+      headless: true,
+    });
+
+    const context = await browser.newContext({
+      userAgent: customUserAgent,
+    });
+
+    const page = await context.newPage();
+    await page.goto(urlWithSlash, { waitUntil: 'networkidle' });
+
+    const content = await page.content();
+
+    await browser.close();
+
+    return content;
+  }
+};
 
 export class WebPageLoader extends DatasourceLoaderBase {
   getSize = async () => {
@@ -26,15 +64,12 @@ export class WebPageLoader extends DatasourceLoaderBase {
 
     const { load } = await import('cheerio');
 
-    const res = await axios(addSlashUrl(url), {
-      headers: {
-        'User-Agent': Date.now().toString(),
-      },
-    });
-    const $ = load(res.data);
+    const content = await loadPageContent(url);
+    const $ = load(content);
     $('script').remove();
     $('style').remove();
     $('link').remove();
+    $('svg').remove();
     const text = $('body').text();
 
     return {
