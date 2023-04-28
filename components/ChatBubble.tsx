961c8d14006c3bd817ebe860dee1b789b35e2728
Georges Petrov <georgesm.petrov@gmail.com>
2023-04-28 23:53:12 +0200

    fix wrong api url

--- components/ChatBubble.tsx
+++ components/ChatBubble.tsx
@@ -61,6 +61,8 @@ const defaultChatBubbleConfig: AgentInterfaceConfig = {
   // messageTemplates: ["What's the pricing?"],
 };
 
+const API_URL = process.env.NEXT_PUBLIC_DASHBOARD_URL;
+
 function App(props: { agentId: string; initConfig?: AgentInterfaceConfig }) {
   const [isOpen, setIsOpen] = React.useState(false);
   const [agent, setAgent] = React.useState<Agent | undefined>();
@@ -82,7 +84,7 @@ function App(props: { agentId: string; initConfig?: AgentInterfaceConfig }) {
   const handleFetchAgent = async () => {
     try {
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_DASHBOARD_URL}/api/external/agents/${props.agentId}`
+        `${API_URL}/api/external/agents/${props.agentId}`
       );
       const data = (await res.json()) as Agent;
 
@@ -119,86 +121,89 @@ function App(props: { agentId: string; initConfig?: AgentInterfaceConfig }) {
       class RetriableError extends Error {}
       class FatalError extends Error {}
 
-      await fetchEventSource(`/api/external/agents/${props.agentId}/query`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          // Accept: 'text/event-stream',
-        },
-        body: JSON.stringify({
-          streaming: true,
-          query: message,
-        }),
-        signal: ctrl.signal,
+      await fetchEventSource(
+        `${API_URL}/api/external/agents/${props.agentId}/query`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            // Accept: 'text/event-stream',
+          },
+          body: JSON.stringify({
+            streaming: true,
+            query: message,
+          }),
+          signal: ctrl.signal,
 
-        async onopen(response) {
-          if (
-            response.ok &&
-            response.headers.get('content-type') === EventStreamContentType
-          ) {
-            return; // everything's good
-          } else if (
-            response.status >= 400 &&
-            response.status < 500 &&
-            response.status !== 429
-          ) {
-            if (response.status === 402) {
-              throw new ApiError(ApiErrorType.USAGE_LIMIT);
-            }
-            // client-side errors are usually non-retriable:
-            throw new FatalError();
-          } else {
-            throw new RetriableError();
-          }
-        },
-        onclose() {
-          // if the server closes the connection unexpectedly, retry:
-          throw new RetriableError();
-        },
-        onerror(err) {
-          console.log('on error', err, Object.keys(err));
-          if (err instanceof FatalError) {
-            ctrl.abort();
-            throw err; // rethrow to stop the operation
-          } else if (err instanceof ApiError) {
-            console.log('ApiError', ApiError);
-            throw err;
-          } else {
-            // do nothing to automatically retry. You can also
-            // return a specific retry interval here.
-          }
-        },
-
-        onmessage: (event) => {
-          if (event.data === '[DONE]') {
-            ctrl.abort();
-          } else if (event.data?.startsWith('[ERROR]')) {
-            ctrl.abort();
-
-            setMessages([
-              ...history,
-              {
-                from: 'agent',
-                message: event.data.replace('[ERROR]', ''),
-              } as any,
-            ]);
-          } else {
-            // const data = JSON.parse(event.data || `{}`);
-            buffer += event.data as string;
-            console.log(buffer);
-
-            const h = [...history];
-
-            if (h?.[nextIndex]) {
-              h[nextIndex].message = `${buffer}`;
+          async onopen(response) {
+            if (
+              response.ok &&
+              response.headers.get('content-type') === EventStreamContentType
+            ) {
+              return; // everything's good
+            } else if (
+              response.status >= 400 &&
+              response.status < 500 &&
+              response.status !== 429
+            ) {
+              if (response.status === 402) {
+                throw new ApiError(ApiErrorType.USAGE_LIMIT);
+              }
+              // client-side errors are usually non-retriable:
+              throw new FatalError();
             } else {
-              h.push({ from: 'agent', message: buffer });
+              throw new RetriableError();
             }
+          },
+          onclose() {
+            // if the server closes the connection unexpectedly, retry:
+            throw new RetriableError();
+          },
+          onerror(err) {
+            console.log('on error', err, Object.keys(err));
+            if (err instanceof FatalError) {
+              ctrl.abort();
+              throw err; // rethrow to stop the operation
+            } else if (err instanceof ApiError) {
+              console.log('ApiError', ApiError);
+              throw err;
+            } else {
+              // do nothing to automatically retry. You can also
+              // return a specific retry interval here.
+            }
+          },
 
-            setMessages(h as any);
-          }
-        },
-      });
+          onmessage: (event) => {
+            if (event.data === '[DONE]') {
+              ctrl.abort();
+            } else if (event.data?.startsWith('[ERROR]')) {
+              ctrl.abort();
+
+              setMessages([
+                ...history,
+                {
+                  from: 'agent',
+                  message: event.data.replace('[ERROR]', ''),
+                } as any,
+              ]);
+            } else {
+              // const data = JSON.parse(event.data || `{}`);
+              buffer += event.data as string;
+              console.log(buffer);
+
+              const h = [...history];
+
+              if (h?.[nextIndex]) {
+                h[nextIndex].message = `${buffer}`;
+              } else {
+                h.push({ from: 'agent', message: buffer });
+              }
+
+              setMessages(h as any);
+            }
+          },
+        }
+      );
     } catch (err) {
       console.log('err', err);
       if (err instanceof ApiError) {
