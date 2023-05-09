b231c45264db54ee44062bf0ac77213493cbee59
serge <sergewilfried@outlook.fr>
2023-05-09 12:48:22 +0000

    🔧 chore(_document.tsx): update Hotjar hjid to 3484092

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -52,7 +52,7 @@ class CustomDocument extends Document {
           <Script id="hotjar" strategy="afterInteractive">
             {`(function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3480619,hjsv:6};
+        h._hjSettings={hjid:3484092,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
