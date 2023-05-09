ad6cd31880b308ca0cf9a6894413194f82c83ba1
serge <sergewilfried@outlook.fr>
2023-05-09 13:08:43 +0000

    🔥 chore(index.tsx): remove unused getStaticProps function from index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -89,12 +89,3 @@ export default function Home() {
   );
 }
 
-export async function getStaticProps({ locale }: GetStaticPropsContext) {
-  return {
-    props: {
-      // You can get the messages from anywhere you like. The recommended pattern
-      // is to put them in JSON files separated by locale (e.g. `en.json`).
-      messages: (await import(`../public/locales/${locale}.json`)).default,
-    },
-  };
-}
