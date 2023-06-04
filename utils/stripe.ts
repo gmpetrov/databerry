8cfe94a55c9d6ab3d39b0c8cf95e318f89314560
loft218 <lifeonfoot@qq.com>
2023-06-04 14:11:07 +0800

    接入stripe

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -1,5 +1,7 @@
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2022-11-15',
-});
+// export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+//   apiVersion: '2022-11-15',
+// });
+
+export const stripe = new Stripe('sk_test_51NF6J3KolXEHwg6BJ2oJakcoPaNn1urc1aCAUskhsqemKUA1WPFYgjEZUn1aJxxVsoRwLQi9rj7NdYxNvprlWhiz00px5y1Sz5', { apiVersion: '2022-11-15', });
