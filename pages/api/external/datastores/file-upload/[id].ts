30f33b71d81aa593910df25908e8bc619d0af58f
Georges Petrov <georgesm.petrov@gmail.com>
2023-06-09 17:08:11 +0200

    feature: Databerry API Upload single endpoint

--- pages/api/external/datastores/file-upload/[id].ts
+++ pages/api/external/datastores/file-upload/[id].ts
@@ -1,22 +1,28 @@
 import {
   DatasourceStatus,
   DatasourceType,
-  DatastoreVisibility,
   SubscriptionPlan,
   Usage,
 } from '@prisma/client';
 import Cors from 'cors';
+import mime from 'mime-types';
+import multer from 'multer';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { z } from 'zod';
 
-import { AppNextApiRequest } from '@app/types/index';
+import {
+  AcceptedDatasourceMimeTypes,
+  AppNextApiRequest,
+} from '@app/types/index';
+import accountConfig from '@app/utils/account-config';
 import { ApiError, ApiErrorType } from '@app/utils/api-error';
+import { s3 } from '@app/utils/aws';
 import { createApiHandler, respond } from '@app/utils/createa-api-handler';
+import generateFunId from '@app/utils/generate-fun-id';
 import guardDataProcessingUsage from '@app/utils/guard-data-processing-usage';
 import prisma from '@app/utils/prisma-client';
 import runMiddleware from '@app/utils/run-middleware';
 import triggerTaskLoadDatasource from '@app/utils/trigger-task-load-datasource';
-import validate from '@app/utils/validate';
 
 const cors = Cors({
   methods: ['POST', 'HEAD'],
@@ -24,37 +30,49 @@ const cors = Cors({
 
 const handler = createApiHandler();
 
-const Schema = z.object({
-  id: z.string().cuid(),
+const FileSchema = z.object({
+  mimetype: z.enum(AcceptedDatasourceMimeTypes),
+  fieldname: z.string(),
+  originalname: z.string(),
+  encoding: z.string(),
+  size: z.number(),
+  buffer: z.any(),
 });
 
-export const fileUpload = async (
-  req: AppNextApiRequest,
-  res: NextApiResponse
-) => {
-  const datastoreId = req.query.id as string;
+export const upload = async (req: AppNextApiRequest, res: NextApiResponse) => {
+  const file = (req as any).file as z.infer<typeof FileSchema>;
 
-  const data = req.body as z.infer<typeof Schema>;
-  const datasourceId = data?.id;
+  try {
+    await FileSchema.parseAsync(file);
+  } catch (err) {
+    console.log('Error File Upload', err);
+    throw new ApiError(ApiErrorType.INVALID_REQUEST);
+  }
+
+  const datastoreId = req.query.id as string;
 
   // get Bearer token from header
   const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(' ')?.[1];
+  const token = authHeader && authHeader?.split(' ')?.[1];
+
+  if (!token) {
+    throw new ApiError(ApiErrorType.UNAUTHORIZED);
+  }
 
   if (!datastoreId) {
     throw new ApiError(ApiErrorType.INVALID_REQUEST);
   }
 
-  const datasource = await prisma.appDatasource.findUnique({
+  const datastore = await prisma.datastore.findUnique({
     where: {
-      id: datasourceId,
+      id: datastoreId,
     },
     include: {
-      datastore: true,
+      apiKeys: true,
       owner: {
         include: {
-          usage: true,
           apiKeys: true,
+          usage: true,
           subscriptions: {
             where: {
               status: 'active',
@@ -65,29 +83,66 @@ export const fileUpload = async (
     },
   });
 
-  if (!datasource) {
+  if (!datastore) {
     throw new ApiError(ApiErrorType.NOT_FOUND);
   }
 
-  if (
-    datasource?.datastoreId !== datastoreId ||
-    (datasource?.datastore?.visibility === DatastoreVisibility.private &&
-      (!token ||
-        !datasource?.owner?.apiKeys.find((each) => each.key === token)))
-  ) {
+  if (!token || !datastore?.owner?.apiKeys.find((each) => each.key === token)) {
     throw new ApiError(ApiErrorType.UNAUTHORIZED);
   }
 
+  const plan =
+    datastore?.owner?.subscriptions?.[0]?.plan || SubscriptionPlan.level_0;
+
+  if (file.size > accountConfig[plan]?.limits?.maxFileSize) {
+    throw new ApiError(ApiErrorType.USAGE_LIMIT);
+  }
+
   guardDataProcessingUsage({
-    usage: datasource?.owner?.usage as Usage,
-    plan:
-      datasource?.owner?.subscriptions?.[0]?.plan || SubscriptionPlan.level_0,
+    usage: datastore?.owner?.usage as Usage,
+    plan,
   });
 
+  const datasource = await prisma.appDatasource.create({
+    data: {
+      type: DatasourceType.file,
+      name: file.originalname || generateFunId(),
+      config: {
+        type: file.mimetype,
+      },
+      status: DatasourceStatus.pending,
+      owner: {
+        connect: {
+          id: datastore?.ownerId!,
+        },
+      },
+      datastore: {
+        connect: {
+          id: datastoreId,
+        },
+      },
+    },
+  });
+
+  // Add to S3
+  const fileExt = mime.extension(file.mimetype);
+  const fileName = `${datasource.id}${fileExt ? `.${fileExt}` : ''}`;
+
+  const params = {
+    Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+    Key: `datastores/${datastore.id}/${fileName}`,
+    Body: file.buffer,
+    ContentType: file.mimetype,
+    ACL: 'public-read',
+  };
+
+  await s3.putObject(params).promise();
+
+  // Trigger processing
   await triggerTaskLoadDatasource([
     {
       userId: datasource.ownerId!,
-      datasourceId,
+      datasourceId: datasource.id,
       priority: 1,
     },
   ]);
@@ -95,12 +150,13 @@ export const fileUpload = async (
   return datasource;
 };
 
-handler.post(
-  validate({
-    body: Schema,
-    handler: respond(fileUpload),
-  })
-);
+handler.use(multer().single('file')).post(respond(upload));
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 export default async function wrapper(
   req: NextApiRequest,
--- types/dtos.ts
+++ types/dtos.ts
@@ -148,17 +148,19 @@ export const UpsertAgentSchema = z.object({
 
 export type UpsertAgentSchema = z.infer<typeof UpsertAgentSchema>;
 
+export const AcceptedDatasourceMimeTypes = [
+  'text/csv',
+  'text/plain',
+  'text/markdown',
+  'application/pdf',
+  'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+] as const;
+
 export const GenerateUploadLinkRequest = z.object({
   fileName: z.string(),
-  type: z.enum([
-    'text/csv',
-    'text/plain',
-    'text/markdown',
-    'application/pdf',
-    'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-  ]),
+  type: z.enum(AcceptedDatasourceMimeTypes),
 });
 
 export type GenerateUploadLinkRequest = z.infer<
