7ccf6ee1f5b60bdd8870f534354f64e3b08b658f
Ramzi H <18570300+rhijjawi@users.noreply.github.com>
2023-07-12 10:03:00 +0200

    AWS Region ID (#59)
    * Update FileForm.tsx
    
    * Add region compatibility to aws.ts
    
    * Add region env variable to .env.example
    
    * Revert FileForm.tsx
    
    ---------
    
    Co-authored-by: Georges Petrov <georgesm.petrov@gmail.com>

--- utils/aws.ts
+++ utils/aws.ts
@@ -4,6 +4,7 @@ export const s3 = new S3({
   signatureVersion: 'v4',
   accessKeyId: process.env.APP_AWS_ACCESS_KEY,
   secretAccessKey: process.env.APP_AWS_SECRET_KEY,
+  region: process.env.APP_AWS_REGION,
   ...(process.env.NEXT_PUBLIC_AWS_ENDPOINT
     ? {
         endpoint: process.env.NEXT_PUBLIC_AWS_ENDPOINT,
