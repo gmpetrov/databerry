a2ae4b0e0a23a9aacc4102758e987f4a8fbf5507
odapX <yahya_benzha@um5.ac.ma>
2023-09-08 16:54:36 +0200

    fix: 🐛 feedbacks

--- components/PartnerLogos.tsx
+++ components/PartnerLogos.tsx
@@ -1,41 +1,78 @@
 import Image from 'next/image'
 import React from 'react';
 
-const companies =  [
-  { name: 'bellman', logoUrl: '/landing-page/company-logos/bellman.jfif' },
-  { name: 'klarna', logoUrl: '/landing-page/company-logos/klarna.jfif' },
-  { name: 'pacificlake', logoUrl: '/landing-page/company-logos/pacificlake.jfif' },
-  { name: 'patreon', logoUrl: '/landing-page/company-logos/patreon.jfif' },
-  { name: 'maskex', logoUrl: '/landing-page/company-logos/maskex.jfif' },
-  { name: 'bnp-paribas', logoUrl: '/landing-page/company-logos/bnp-paribas.jfif' },
-  { name: 'ebc', logoUrl: '/landing-page/company-logos/ebc.png' },
-  { name: 'pmu', logoUrl: '/landing-page/company-logos/pmu.jfif' },
-  { name: 'bcg', logoUrl: '/landing-page/company-logos/bcg.jfif' },
-  { name: 'advicci', logoUrl: '/landing-page/company-logos/abu-dhabi-chamber.jfif' },
-  { name: 'everbreed', logoUrl: '/landing-page/company-logos/everbreed.webp' },
-  { name: 'my-operator', logoUrl: '/landing-page/company-logos/my-operator.jfif' },
-]
-
 export default function PartnerLogos() {
     return (
       <div className="bg-black py-24 sm:py-32">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <h2 className="text-center text-lg font-semibold leading-8 text-white">
-            Trusted by the world’s most innovative teams
+        <div className="mx-auto flex flex-col justify-center max-w-7xl px-6 lg:px-8">
+          <h2 className="text-center text-xl font-display leading-12 text-white pb-9">
+          Earning the trust of the world&apos;s most innovative teams
           </h2>
-          <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-            {companies.map((company)=>(
-              <div className="relative flex justify-center"  key={company.name}>
-              <Image
-                 loading="lazy"
-                 className="object-contain lg:col-span-1"
-                 src={company.logoUrl}
-                 alt={company.name}
-                 width={90}
-                 height={40}
-               />
-              </div>
-            ))}
+          <div className="mx-auto mt-10 grid grid-cols-1 place-items-center xs:grid-cols-2 items-center gap-x-10 gap-y-10  sm:grid-cols-3 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+            <Image
+              loading="lazy"
+              className="object-contain lg:col-span-1"
+              src="/landing-page/company-logos/klarna.png"
+              alt="Klarna Logo"
+              width={130}
+              height={30}
+            />
+            <Image
+              loading="lazy"
+              className="object-contain lg:col-span-1"
+              src="/landing-page/company-logos/bcg.png"
+              alt="BCG Logo"
+              width={80}
+              height={60}
+            />
+            <Image
+              loading="lazy"
+              className="object-contain lg:col-span-1"
+              src="/landing-page/company-logos/bnp-paribas.png"
+              alt="BNP Paribas Logo"
+              width={200}
+              height={150}
+            />
+            <Image
+              loading="lazy"
+              className="object-contain lg:col-span-1"
+              src="/landing-page/company-logos/bellman.svg"
+              alt="Bellman Logo"
+              width={180}
+              height={90}
+            />
+             <Image
+              loading="lazy"
+              className="object-contain lg:col-span-1"
+              src="/landing-page/company-logos/patreon.png"
+              alt="Patreon Logo"
+              width={155}
+              height={70}
+            />      
+            <Image
+              loading="lazy"
+              className="object-contain lg:col-span-1"
+              src="/landing-page/company-logos/maskex.png"
+              alt="Maskex Logo"
+              width={140}
+              height={60}
+            />
+            <Image
+              loading="lazy"
+              className="object-contain lg:col-span-1"
+              src="/landing-page/company-logos/pmu.jpg"
+              alt="PMU Logo"
+              width={130}
+              height={40}
+            />
+            <Image
+              loading="lazy"
+              className="object-contain lg:col-span-1"
+              src="/landing-page/company-logos/abu-dhabi-chamber.png"
+              alt="Abu Dhabi Chamber Logo"
+              width={180}
+              height={90}
+            />
           </div>
         </div>
       </div>
