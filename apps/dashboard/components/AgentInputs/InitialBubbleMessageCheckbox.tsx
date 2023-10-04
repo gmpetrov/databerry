d7de323bb985cd625424c5b6ff1ddc247ca522dd
yahyha benzha <yahya.benzha1@gmail.com>
2023-10-04 14:46:24 +0100

    feat: 🎸 Disable init message on bubble

---
+++ apps/dashboard/components/AgentInputs/InitialBubbleMessageCheckbox.tsx
@@ -0,0 +1,28 @@
+import { Checkbox, Stack, Typography } from "@mui/joy";
+import { useFormContext } from "react-hook-form";
+
+function InitialBubbleMessageCheckbox({ labelText = "Hide initial message on chat bubble" }) {
+    const { register, setValue, getValues } = useFormContext();
+    const { interfaceConfig } = getValues()
+    const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue('interfaceConfig.isInitMessagePopupDisabled', e.target.checked, {
+            shouldDirty: true,
+            shouldValidate: true,
+        });
+    };
+    return (
+        <Stack direction="row" spacing={1} alignItems="center">
+            <Checkbox
+                {...register('interfaceConfig.isInitMessagePopupDisabled')}
+                onChange={handleCheckboxChange}
+                checked={Boolean(interfaceConfig?.isInitMessagePopupDisabled)}
+                aria-label={labelText}
+            />
+            <Typography>{labelText}</Typography>
+        </Stack>
+    );
+}
+
+
+
+export default InitialBubbleMessageCheckbox;
--- apps/dashboard/components/ChatBubble.tsx
+++ apps/dashboard/components/ChatBubble.tsx
@@ -140,7 +140,7 @@ function App(props: { agentId: string; initConfig?: AgentInterfaceConfig }) {
   }, [props.agentId]);
 
   useEffect(() => {
-    if (state.config?.initialMessage) {
+    if (state.config?.initialMessage && !state.config?.isInitMessagePopupDisabled) {
       setTimeout(() => {
         setState({
           showInitialMessage: true,
