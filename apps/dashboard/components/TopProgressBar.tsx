ae1890f877b9f3c4ad1b7f1ad7e1ae8ec342c9a5
yahyha benzha <yahya.benzha1@gmail.com>
2023-10-26 11:57:01 +0100

    fix: 🐛 loading issues

--- apps/dashboard/components/TopProgressBar.tsx
+++ apps/dashboard/components/TopProgressBar.tsx
@@ -1,59 +1,7 @@
-import Router from 'next/router';
-import NProgress from 'nprogress';
-
-let timer: any;
-let state: any;
-let activeRequests = 0;
-const delay = 250;
-
-function load() {
-  if (state === 'loading') {
-    return;
-  }
-
-  state = 'loading';
-
-  timer = setTimeout(function () {
-    NProgress.start();
-  }, delay); // only show progress bar if it takes longer than the delay
-}
-
-function stop() {
-  if (activeRequests > 0) {
-    return;
-  }
-
-  state = 'stop';
-
-  clearTimeout(timer);
-  NProgress.done();
-}
-
-Router.events.on('routeChangeStart', load);
-Router.events.on('routeChangeComplete', stop);
-Router.events.on('routeChangeError', stop);
-
-const originalFetch = window.fetch;
-window.fetch = async function (...args) {
-  if (activeRequests === 0) {
-    load();
-  }
-
-  activeRequests++;
-
-  try {
-    const response = await originalFetch(...args);
-    return response;
-  } catch (error) {
-    return Promise.reject(error);
-  } finally {
-    activeRequests -= 1;
-    if (activeRequests === 0) {
-      stop();
-    }
-  }
-};
+import useNpgrogress from '@app/hooks/useNprogress';
 
 export default function TopProgressBar() {
+  useNpgrogress();
+
   return null;
 }
---
+++ apps/dashboard/hooks/useNprogress.ts
@@ -0,0 +1,24 @@
+import { useRouter } from 'next/router';
+import NProgress from 'nprogress';
+import { useEffect } from 'react';
+
+export default function useNpgrogress() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = () => NProgress.start();
+    const handleStop = () => NProgress.done();
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router.events]);
+
+  return {};
+}
