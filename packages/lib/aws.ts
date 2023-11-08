d518d43f63467016e96b3b6610dc4ce95deba1e9
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-11-08 08:25:17 +0000

    chore: format

--- packages/lib/aws.ts
+++ packages/lib/aws.ts
@@ -1,23 +1,17 @@
-import { S3 } from "@aws-sdk/client-s3";
+import { S3 } from '@aws-sdk/client-s3';
 
 export const s3 = new S3({
   credentials: {
     accessKeyId: process.env.APP_AWS_ACCESS_KEY,
     secretAccessKey: process.env.APP_AWS_SECRET_KEY
   },
-
-  // The key signatureVersion is no longer supported in v3, and can be removed.
-  // @deprecated SDK v3 only supports signature v4.
-  signatureVersion: 'v4',
-
   region: process.env.APP_AWS_REGION,
-
   ...(process.env.APP_AWS_S3_ENDPOINT
     ? {
         endpoint: process.env.APP_AWS_S3_ENDPOINT,
-        s3ForcePathStyle: process.env.APP_AWS_S3_FORCE_PATH_STYLE === 'true',
+        forcePathStyle: process.env.APP_AWS_S3_FORCE_PATH_STYLE === 'true',
       }
-    : {})
+    : {}),
 });
 
 export async function deleteFolderFromS3Bucket(
