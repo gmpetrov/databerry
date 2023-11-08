6a8236a63fe4c7a6dc583f836c487ea0f7e3665b
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-11-08 08:23:18 +0000

    chore: run codemod on packages/lib/s3.ts

--- packages/lib/aws.ts
+++ packages/lib/aws.ts
@@ -1,16 +1,23 @@
-import { S3 } from 'aws-sdk';
+import { S3 } from "@aws-sdk/client-s3";
 
 export const s3 = new S3({
+  credentials: {
+    accessKeyId: process.env.APP_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.APP_AWS_SECRET_KEY
+  },
+
+  // The key signatureVersion is no longer supported in v3, and can be removed.
+  // @deprecated SDK v3 only supports signature v4.
   signatureVersion: 'v4',
-  accessKeyId: process.env.APP_AWS_ACCESS_KEY,
-  secretAccessKey: process.env.APP_AWS_SECRET_KEY,
+
   region: process.env.APP_AWS_REGION,
+
   ...(process.env.APP_AWS_S3_ENDPOINT
     ? {
         endpoint: process.env.APP_AWS_S3_ENDPOINT,
         s3ForcePathStyle: process.env.APP_AWS_S3_FORCE_PATH_STYLE === 'true',
       }
-    : {}),
+    : {})
 });
 
 export async function deleteFolderFromS3Bucket(
@@ -22,7 +29,7 @@ export async function deleteFolderFromS3Bucket(
     Prefix: prefix,
   };
 
-  const listedObjects = await s3.listObjectsV2(listParams).promise();
+  const listedObjects = await s3.listObjectsV2(listParams);
 
   if (listedObjects?.Contents?.length === 0) return;
 
@@ -35,5 +42,5 @@ export async function deleteFolderFromS3Bucket(
     deleteParams.Delete.Objects.push({ Key });
   });
 
-  return s3.deleteObjects(deleteParams).promise();
+  return s3.deleteObjects(deleteParams);
 }
