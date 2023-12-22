00a4365f6b8922051fd95c741a2fbb86fc74ec74
Georges Petrov <georgesm.petrov@gmail.com>
2023-12-22 13:34:12 +0100

    WIP

--- apps/dashboard/components/MotionBottom.tsx
+++ apps/dashboard/components/MotionBottom.tsx
@@ -1,32 +1,26 @@
 import React, { useRef } from 'react';
 
-import Motion, { Props as MotionProps } from './Motion';
+import Motion, { Props as CustomMotionProps } from './Motion';
 
-type Props = MotionProps & {};
+type Props = CustomMotionProps & {};
 
 function MotionBottom({ ...otherProps }: Props) {
   return React.createElement(Motion, {
-    ...otherProps,
     initial: {
       opacity: 0,
       translateY: '100px',
-      ...(typeof otherProps.initial === 'object' ? otherProps.initial : {}),
     },
     animate: {
       opacity: 1,
       translateY: '0px',
-      ...(typeof otherProps.animate === 'object' ? otherProps.animate : {}),
     },
     exit: {
       opacity: 0,
-      ...(typeof otherProps.exit === 'object' ? otherProps.exit : {}),
     },
     transition: {
-      duration: 0.2,
-      ...(typeof otherProps.transition === 'object'
-        ? otherProps.transition
-        : {}),
+      duration: 0.15,
     },
+    ...otherProps,
   });
 }
 
