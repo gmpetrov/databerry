45f388a6a2a6dce8f7ff2b67b2040425223888ef
Georges Petrov <georgesm.petrov@gmail.com>
2024-01-18 14:01:31 +0100

    fix: 🐛 react-markdown crash + open links in new window

--- apps/dashboard/components/Markdown.tsx
+++ apps/dashboard/components/Markdown.tsx
@@ -1,42 +1,40 @@
+import 'property-information'; // https://github.com/remarkjs/react-markdown/issues/747#issuecomment-1674799817
+
 import clsx from 'clsx';
 import React, { useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
 // import ReactMarkdown from 'react-markdown';
 import { useRemark } from 'react-remark';
+import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
 
 type Props = any & {};
 
 function Markdown({ children, className, ...otherProps }: Props) {
-  // Using useRemark hook as a workaround for the Shadow dom bug with <ReactMarkdown /> component
-  const [reactContent, setMarkdownSource] = useRemark({
-    remarkPlugins: [],
-  });
-
-  useEffect(() => {
-    setMarkdownSource(children);
-  }, [children]);
+  if (!children) {
+    return null;
+  }
 
   return (
-    // <ReactMarkdown
-    //   className={clsx('prose-sm prose dark:prose-invert', className)}
-    //   remarkPlugins={[remarkGfm]}
-    //   // linkTarget={'_blank'}
-    //   {...otherProps}
-    //   sourc
-    // >
-    //   {children}
-    //   {/* ![image](https://i.giphy.com/media/d8L0LCmpEG1aTILGVL/giphy.gif) */}
-    // </ReactMarkdown>
-
-    <div
+    <ReactMarkdown
+      {...otherProps}
       className={clsx(
         'prose-sm prose dark:prose-invert',
         'prose-p:leading-relaxed prose-pre:p-0 break-words text-sm',
         className
       )}
+      components={{
+        a: ({ href, children }) => (
+          <a href={href} target="_blank" rel="noopener noreferrer">
+            {children}
+          </a>
+        ),
+      }}
+      remarkPlugins={[remarkGfm]}
+      rehypePlugins={[rehypeRaw]}
     >
-      {reactContent}
-    </div>
+      {children}
+    </ReactMarkdown>
   );
 }
 
