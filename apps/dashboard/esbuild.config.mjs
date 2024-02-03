3cf141595aac8d91716f7addab6882a2232701ec
Georges Petrov <georgesm.petrov@gmail.com>
2024-02-03 08:45:46 +0100

    WIP

--- apps/dashboard/esbuild.config.mjs
+++ apps/dashboard/esbuild.config.mjs
@@ -1,11 +1,12 @@
 import * as esbuild from 'esbuild';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 let ctx = await esbuild.context({
   entryPoints: ['widgets/chatbox/index.ts', 'widgets/chatbox/styles.ts'],
   bundle: true,
   outdir: 'tests/chatbox/dist',
   external: ['fs', 'stream', 'zlib', 'process'],
-  // minify: true,
   allowOverwrite: true,
   format: 'esm',
   jsx: 'automatic',
@@ -13,9 +14,44 @@ let ctx = await esbuild.context({
     'process.env': '{}',
     'process.env.NEXT_PUBLIC_ASSETS_BASE_URL': '"/dist"',
     'process.env.NEXT_PUBLIC_DASHBOARD_URL': '"http://localhost:3000"',
-    // 'process.env.NEXT_PUBLIC_ASSETS_BASE_URL': '"https://cdn.jsdelivr.net/npm/@databerry/chat-bubble@latest NEXT_PUBLIC_DASHBOARD_URL=https://app.chaindesk.ai"',
+
+    ...(isProd
+      ? {
+          'process.env.NODE_ENV': '"production"',
+          'process.env.NEXT_PUBLIC_ASSETS_BASE_URL':
+            '"https://cdn.jsdelivr.net/npm/@databerry/chat-bubble@latest"',
+          'process.env.NEXT_PUBLIC_DASHBOARD_URL': '"https://app.chaindesk.ai"',
+        }
+      : {}),
   },
+  metafile: true,
+  sourcemap: true,
+  ...(isProd
+    ? {
+        minify: true,
+        sourcemap: false,
+      }
+    : {}),
 });
 
-await ctx.watch();
-console.log('watching...');
+if (isProd) {
+  const b = await ctx.rebuild();
+
+  console.log(
+    await esbuild.analyzeMetafile(b.metafile, {
+      verbose: true,
+    })
+  );
+
+  await ctx.dispose();
+  console.log('✅ Build complete!');
+} else {
+  let { host, port } = await ctx.serve({
+    servedir: 'tests/chatbox',
+  });
+  console.log(
+    `🚀 Server running at http://${
+      host === '0.0.0.0' ? 'localhost' : host
+    }:${port}/`
+  );
+}
