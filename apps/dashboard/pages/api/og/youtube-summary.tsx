57d56eaa7b2567e1143ad26f8bb36e582fbdaead
Georges Petrov <georgesm.petrov@gmail.com>
2024-02-25 14:53:44 +0100

    fix: 🐛 og image yt summary

--- apps/dashboard/pages/api/og/youtube-summary.tsx
+++ apps/dashboard/pages/api/og/youtube-summary.tsx
@@ -24,8 +24,8 @@ export default async function handler(request: NextRequest) {
     videoThumbnail: string;
   };
 
-  const isFallback =
-    !state?.title || !state?.channelThumbnail || !state?.videoThumbnail;
+  // const isFallback =
+  //   !state?.title || !state?.channelThumbnail || !state?.videoThumbnail;
 
   return new ImageResponse(
     (
@@ -41,68 +41,18 @@ export default async function handler(request: NextRequest) {
           fontFamily: 'Josefin Sans',
         }}
       >
-        {isFallback && (
-          <div tw="flex flex-col w-full h-full justify-center items-center">
+        <>
+          <div tw="flex relative flex-col w-full h-full">
             <img
-              src={`https://chaindesk.ai/app-logo-light.png`}
-              alt="Chaindesk Logo"
-              style={{
-                width: 977 * 0.4,
-                height: 179 * 0.4,
-              }}
-              width={977}
-              height={179}
+              src={state?.videoThumbnail}
+              alt="Video thumbnail"
+              tw="w-full h-full"
+              style={{ objectFit: 'cover' }}
+              width={1000}
+              height={1000}
             />
-
-            <h1 tw="text-7xl mt-12">Free AI Youtube Summarizer</h1>
           </div>
-        )}
-        {!isFallback && (
-          <>
-            <div tw="w-1/2 flex h-full p-2 border-8 border-black">
-              <div tw="flex flex-col justify-center items-center w-full text-center">
-                <div tw="flex flex-col w-full h-full justify-center items-center">
-                  {/* <div tw="flex w-52 h-52 rounded-full border-8 border-black overflow-hidden "> */}
-                  {/* <img
-                      src={state?.channelThumbnail}
-                      alt="Video thumbnail"
-                      tw="w-42 h-auto aspect-squqre"
-                      width={100}
-                      height={100}
-                    />
-                  </div> */}
-
-                  <h1 tw="flex flex-col text-2xl sm:text-3xl font-bold text-gray-900 justify-center">
-                    <span>{state.title}</span>
-                  </h1>
-                </div>
-
-                {/* <div tw="flex flex-col w-full h-1/2 justify-center items-center bg-gray-100">
-              <img
-                src={`https://chaindesk.ai/app-logo-light.png`}
-                alt="Chaindesk Logo"
-                style={{
-                  width: 977 * 0.3,
-                  height: 179 * 0.3,
-                }}
-                width={977}
-                height={179}
-              />
-
-            </div> */}
-              </div>
-            </div>
-            <div tw="w-1/2 flex h-full  flex-col">
-              <img
-                src={state?.videoThumbnail}
-                alt="Video thumbnail"
-                tw="w-full h-full aspect-auto mt-auto"
-                width={100}
-                height={100}
-              />
-            </div>
-          </>
-        )}
+        </>
       </div>
     ),
     {
