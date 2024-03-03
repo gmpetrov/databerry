69ecf91815ca9679bc85e642da81df74eb2da3a7
Georges Petrov <georgesm.petrov@gmail.com>
2024-03-04 00:22:48 +0100

    WIP

--- apps/lp/app/(default)/tools/youtube-summarizer/page.tsx
+++ apps/lp/app/(default)/tools/youtube-summarizer/page.tsx
@@ -45,7 +45,7 @@ export default async function YoutubeVideoSummarizer() {
       <section className="relative before:absolute before:inset-0 before:h-80 before:pointer-events-none before:bg-gradient-to-b before:from-zinc-100 before:-z-10">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
           <PromoAlert />
-          <YoutubeSummarizerForm />
+          {/* <YoutubeSummarizerForm /> */}
 
           <div className="px-4">
             <LatestVideoSummaries summaries={summaries || []} />
