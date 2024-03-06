747370e75bfe9a986a05bc17e1f641e5640df253
Georges Petrov <georgesm.petrov@gmail.com>
2024-03-06 12:24:12 +0100

    fix: 🐛 next sitemap indexes

--- apps/lp/app/api/sitemaps/route.tsx
+++ apps/lp/app/api/sitemaps/route.tsx
@@ -1,16 +1,7 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-import { youtubeSummaryTool } from '@chaindesk/lib/config';
-import { Prisma } from '@chaindesk/prisma';
-import prisma from '@chaindesk/prisma/client';
-
 export async function GET(req: Request) {
   const baseUrl = process.env.NEXT_PUBLIC_LANDING_PAGE_URL as string;
 
-  const paths = [
-    `${baseUrl}/api/sitemaps/main.xml`,
-    `${baseUrl}/api/sitemaps/tools/youtube-summarizer.xml`,
-  ] as string[];
+  const paths = [`${baseUrl}/api/sitemaps/main.xml`] as string[];
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
       <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
