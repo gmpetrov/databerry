210ade1e2418cda47fd610fec00eeaf9d0c9c513
yahyha benzha <yahya.benzha1@gmail.com>
2024-03-06 12:57:09 +0100

    feat: 🎸 temp shopify impl

--- apps/dashboard/components/AgentDeployTab.tsx
+++ apps/dashboard/components/AgentDeployTab.tsx
@@ -6,7 +6,6 @@ import Button from '@mui/joy/Button';
 import Chip from '@mui/joy/Chip';
 import Stack from '@mui/joy/Stack';
 import Typography from '@mui/joy/Typography';
-import axios from 'axios';
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
@@ -63,6 +62,13 @@ const ZendeskSettings = dynamic(
   }
 );
 
+const ShopifySettings = dynamic(
+  () => import('@app/components/ShopifySettings'),
+  {
+    ssr: false,
+  }
+);
+
 const WhatsAppSettings = dynamic(
   () => import('@app/components/WhatsAppSettings'),
   {
@@ -89,6 +95,7 @@ function AgentDeployTab(props: Props) {
   const standalonePageModal = useModal();
   const zendeskModal = useModal();
   const whatsappModal = useModal();
+  const shopifyModal = useModal();
 
   const { query, mutation } = useAgent({
     id: props.agentId as string,
@@ -156,7 +163,6 @@ function AgentDeployTab(props: Props) {
               },
               publicAgentRequired: true,
             },
-
             {
               hidden: false,
               name: 'WhatsApp',
@@ -171,6 +177,23 @@ function AgentDeployTab(props: Props) {
               ),
               action: async () => {
                 whatsappModal.open();
+              },
+              isPremium: true,
+            },
+            {
+              hidden: false,
+              name: 'Shopify',
+              icon: (
+                <Image
+                  className="w-8"
+                  src="/shopify/icon.svg"
+                  width={100}
+                  height={100}
+                  alt="Shopify Logo"
+                />
+              ),
+              action: async () => {
+                shopifyModal.open();
               },
               isPremium: true,
             },
@@ -426,6 +449,32 @@ function AgentDeployTab(props: Props) {
           >
             <WhatsAppSettings agentId={props.agentId} />
           </whatsappModal.component>
+
+          <shopifyModal.component
+            title={
+              <Typography
+                startDecorator={
+                  <Image
+                    className="w-6"
+                    src="/shopify/icon.svg"
+                    width={100}
+                    height={100}
+                    alt="Whatsapp Logo"
+                  />
+                }
+              >
+                Shopify
+              </Typography>
+            }
+            dialogProps={{
+              sx: {
+                maxWidth: 'md',
+                height: 'auto',
+              },
+            }}
+          >
+            <ShopifySettings agentId={props.agentId} />
+          </shopifyModal.component>
         </>
       )}
 
---
+++ apps/dashboard/components/ShopifySettings.tsx
@@ -0,0 +1,89 @@
+import { Box, Divider, Stack, Typography } from '@mui/joy';
+import Image from 'next/image';
+import React from 'react';
+import toast from 'react-hot-toast';
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import html from 'react-syntax-highlighter/dist/esm/languages/hljs/htmlbars';
+import docco from 'react-syntax-highlighter/dist/esm/styles/hljs/vs2015';
+
+if (typeof window !== 'undefined') {
+  SyntaxHighlighter.registerLanguage('htmlbars', html);
+}
+
+type Props = {
+  agentId: string;
+};
+
+export default function ShopifySettings(props: Props) {
+  const installScript = `<script type="module">
+  import Chatbox from 'https://cdn.jsdelivr.net/npm/@chaindesk/embeds@latest/dist/chatbox/index.js';
+
+  Chatbox.initBubble({
+    agentId: '${props.agentId}',
+     
+    // optionally, Override initial messages
+    // initialMessages: [
+    //   'Hello Georges how are you doing today?',
+    //   'How can I help you ?',
+    // ],
+     
+    // optionally Provided context will be appended to the Agent system prompt
+    // context: "your custom context goes here 🙂",
+  })   
+</script>`;
+
+  return (
+    <Stack gap={3}>
+      <Typography level="title-lg">Installation Steps</Typography>
+      <Stack>
+        <Typography level="body-lg">
+          1- On your shop dashboard, go to theme code edition{' '}
+        </Typography>
+        <Image
+          src="/shopify/theme.png"
+          width={900}
+          height={900}
+          alt="shopify theme navigation guide."
+        />
+      </Stack>
+      <Stack>
+        <Typography level="body-lg">2- Copy the code Below </Typography>
+        <Box
+          sx={{ cursor: 'copy' }}
+          onClick={() => {
+            navigator.clipboard.writeText(installScript);
+            toast.success('Copied!', {
+              position: 'bottom-center',
+            });
+          }}
+        >
+          <SyntaxHighlighter
+            language="htmlbars"
+            style={docco}
+            customStyle={{
+              borderRadius: 10,
+            }}
+          >
+            {installScript}
+          </SyntaxHighlighter>
+        </Box>
+      </Stack>
+      <Stack>
+        <Typography level="body-lg">
+          3- Paste the code just above <code>{'</head>'} </code>in the
+          theme.liquid code.{' '}
+        </Typography>
+        <Image
+          src="/shopify/code.png"
+          width={900}
+          height={900}
+          alt="shopify theme navigation guide."
+        />
+      </Stack>
+      <Stack>
+        <Typography>4- Save your changes 🎉 </Typography>
+      </Stack>
+      <Divider />
+    </Stack>
+  );
+}
