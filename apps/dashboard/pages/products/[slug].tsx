2d0fb885c0ce6e0fa2c0b3bfa04da5d650febf7b
Georges Petrov <georgesm.petrov@gmail.com>
2024-03-06 01:02:51 +0100

    WIP

--- apps/dashboard/pages/products/[slug].tsx
+++ apps/dashboard/pages/products/[slug].tsx
@@ -154,7 +154,7 @@ export function PrimaryFeatures(props: {
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
           <dl className="grid grid-cols-1 gap-x-8 gap-y-10 max-w-xl lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-            {props?.features?.map((feature, idx) => (
+            {props?.features?.items?.map((feature, idx) => (
               <div key={feature.name} className="relative pl-16">
                 <dt className="text-base font-semibold leading-7 text-gray-100">
                   <Box
