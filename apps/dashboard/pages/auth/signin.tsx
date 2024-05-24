bc646036293c83aa001b7a552cad95c523b77568
Georges Petrov <georgesm.petrov@gmail.com>
2024-05-24 14:48:54 +0200

    fix: 🐛 loading state signin button

--- apps/dashboard/pages/auth/signin.tsx
+++ apps/dashboard/pages/auth/signin.tsx
@@ -74,8 +74,14 @@ export default function SignInPage() {
     defaultValues: {},
   });
 
-  const handleSubmitEmail = (values: Schema) => {
-    signIn('email', { email: values.email });
+  const handleSubmitEmail = async (values: Schema) => {
+    try {
+      setIsLoading(true);
+      await signIn('email', { email: values.email });
+    } catch {
+    } finally {
+      setIsLoading(false);
+    }
   };
   return (
     <>
@@ -99,8 +105,8 @@ export default function SignInPage() {
         })}
       >
         <>
-          <div className="flex flex-col flex-1 justify-center px-4 py-12 sm:px-6 lg:px-20 xl:px-24">
-            <div className="flex mx-auto w-full max-w-sm lg:w-96">
+          <div className="flex flex-col justify-center flex-1 px-4 py-12 sm:px-6 lg:px-20 xl:px-24">
+            <div className="flex w-full max-w-sm mx-auto lg:w-96">
               {!isReady && (
                 <CircularProgress
                   size="sm"
@@ -118,15 +124,15 @@ export default function SignInPage() {
                 // leaveFrom="opacity-100"
                 // leaveTo="opacity-0"
               >
-                <div className="flex flex-col justify-center items-center">
+                <div className="flex flex-col items-center justify-center">
                   <div className="inline-flex items-center mr-auto space-x-2">
                     <a
                       href="https://chaindesk.ai"
                       // className="absolute top-4 left-4 md:top-8 md:left-8"
                     >
                       <div className="inline-flex items-center space-x-2">
-                        <div className="flex justify-center items-center w-8 h-8 bg-transparent rounded shadow-sm shadow-zinc-950/20">
-                          <Logo className="w-14 cursor-pointer" />
+                        <div className="flex items-center justify-center w-8 h-8 bg-transparent rounded shadow-sm shadow-zinc-950/20">
+                          <Logo className="cursor-pointer w-14" />
                         </div>
                         <Typography level="h4" fontWeight="xl">
                           {`Chaindesk`}
@@ -144,10 +150,10 @@ export default function SignInPage() {
                   </div>
                 </div>
 
-                <div className="mt-8 w-full">
-                  <div className="mt-6 w-full">
+                <div className="w-full mt-8">
+                  <div className="w-full mt-6">
                     <form
-                      className="flex flex-col space-y-4 w-full"
+                      className="flex flex-col w-full space-y-4"
                       onSubmit={handleSubmit(handleSubmitEmail)}
                     >
                       <Input
@@ -172,12 +178,12 @@ export default function SignInPage() {
 
                   <div className="mt-8">
                     <div className="relative">
-                      <div className="flex absolute inset-0 justify-center">
+                      <div className="absolute inset-0 flex justify-center">
                         <Divider sx={{ width: '100%', my: 'auto' }} />
 
                         {/* <div className="w-full border-t border-gray-500" /> */}
                       </div>
-                      <div className="flex relative justify-center">
+                      <div className="relative flex justify-center">
                         <Typography
                           level="body-xs"
                           className="px-2"
